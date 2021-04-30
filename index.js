2518c40a454ac7d45e23a73546d699d54994c30e
Naylepsh <naylepsh@gmail.com>
2021-04-30 13:10:16 +0200

    add designer and material

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import { getLinksToProducts } from "./vipp/products.js";
 const main = async () => {
   // const links = await getLinksToProducts();
   // const link = '/no/products/cabin-kjøkkenstol' // those meme letters dont work
-  const link = "/no/products/bademodul-medium-0";
+  // const link = "/no/products/bademodul-medium-0";
+  const link = "/no/products/pedalb%C3%B8tte-4-l";
   const data = await getProduct(link);
   console.log(data);
 };
--- vipp/product.js
+++ vipp/product.js
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
 import { JSDOM } from "jsdom";
 import { supplier, brand, coreUrl } from "./constants.js";
+import { capitalize } from "../utils/capitalize.js";
 
 export const getProduct = async (url) => {
   const res = await fetch(toProductUrl(url));
@@ -9,7 +10,12 @@ export const getProduct = async (url) => {
   const { document } = new JSDOM(text).window;
 
   const summary = document.querySelector(".product-info__flag > div");
-  const data = getProductDataFromSummary(summary);
+  const details = document.querySelector("#product-detail > div > div");
+  const data = {
+    ...getProductDataFromSummary(summary),
+    ...getProductDataFromDetails(details),
+  };
+
   return data;
 };
 
@@ -19,9 +25,9 @@ const toProductUrl = (url) => {
 
 const getProductDataFromSummary = (summary) => {
   const sku = getSku(summary);
-  const [name, variant] = getNameAndVariant(summary);
+  const { name, variant } = getNameAndVariant(summary);
   const description = getDescription(summary);
-  const [price, currency] = getPriceAndCurrency(summary);
+  const { price, currency } = getPriceAndCurrency(summary);
 
   return { sku, brand, supplier, name, variant, description, price, currency };
 };
@@ -35,7 +41,7 @@ const getSku = (summary) => {
 const getNameAndVariant = (summary) => {
   const text = summary.querySelector("h1").textContent;
   const [name, variant] = text.split(", ").map((t) => t.trim());
-  return [name, variant];
+  return { name, variant };
 };
 
 const getDescription = (summary) => {
@@ -45,5 +51,50 @@ const getDescription = (summary) => {
 const getPriceAndCurrency = (summary) => {
   const text = summary.querySelector("p#price-product").textContent;
   const [price, currency] = text.split(" ");
-  return [price, currency];
+  return { price, currency };
+};
+
+const getProductDataFromDetails = (details) => {
+  getDimensions(details);
+  const designer = getDesigner(details);
+  const material = getMaterial(details);
+
+  return { designer, material };
+};
+
+const getDimensions = (details) => {
+  const dimensionsDiv = getRowFromDetails(details, "Dimensjoner");
+
+  const dimensions = dimensionsDiv.children[1].textContent.split(":");
+  // TODO
+};
+
+const getDesigner = (details) => {
+  const designerDiv = getRowFromDetails(details, "Design");
+  if (!designerDiv) return;
+
+  const designer = designerDiv.querySelector("p").textContent.split(",")[0];
+
+  return designer;
+};
+
+const getMaterial = (details) => {
+  const materialDiv = getRowFromDetails(details, "Materialer");
+  if (!materialDiv) return;
+
+  const material = materialDiv
+    .querySelector("p")
+    .textContent.split(".")[0]
+    .replace(/\s+/g, "")
+    .split(",")
+    .map(capitalize)
+    .join("/");
+
+  return material;
+};
+
+const getRowFromDetails = (details, rowName) => {
+  return Array.from(details.querySelectorAll(".line")).find(
+    (element) => element.children[0].textContent === rowName
+  );
 };
